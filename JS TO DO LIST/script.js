3f1d9707ed6192257bdd30814d9e7babba5ad19e
NavyaArora24 <aroranavya78@gmail.com>
2025-06-21 14:49:19 +0500

    done

--- JS TO DO LIST/script.js
+++ JS TO DO LIST/script.js
@@ -1,13 +1,35 @@
 const inputbox = document.querySelector(".text-id");
-const todolist = document.querySelector("# todo");
+const todolist = document.querySelector("#todo");
 
 function addTask(){
+
+    
     if(inputbox.value === ''){
         alert("Please enter a task");
     }
     else{
         const newTask = document.createElement("li");
-        newTask.innerHTML = inputbox.value;
-        todolist.appendChild(li);
+        const checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+
+        newTask.style.listStyleType = "none";
+
+        newTask.appendChild(checkBox);
+        newTask.appendChild(document.createTextNode(" "));
+        newTask.appendChild(document.createTextNode(inputbox.value));
+        
+        todolist.appendChild(newTask);
+        inputbox.value=" ";
+
+        checkBox.addEventListener("change", function(){
+            if(checkBox.checked){
+                newTask.style.textDecoration = "line-through";
+            }
+            else{
+                newTask.style.textDecoration = "none";
+            }
+        })
+
+        
     }
 }
